87652792c0c46559e6846a8b260c35efe285eb67
avelarimrmt <49668773+avelarimrmt@users.noreply.github.com>
2020-04-24 17:46:44 +0500

    Create structure block of results

--- intofficemap/js/cardOfResults.js
+++ intofficemap/js/cardOfResults.js
@@ -17,10 +17,47 @@ function setCardOfResults(employees) {
     // resultsSeparator.src = './images/br-horiz.svg';
 
 
-    const block=document.createElement('div');
+    const block = document.createElement('div');
     block.classList='block-result';
     results.appendChild(block);
+        const rowRes = document.createElement('div');
+        rowRes.classList='row-res';
+        block.appendChild(rowRes);
+            const name = document.createElement('span');
+            const position = document.createElement('span');
+            name.classList='res-name';
+            position.classList='res-pos';
+            rowRes.appendChild(name);
+            rowRes.appendChild(position);
 
+            const unchangeStroke = document.createElement('div');
+            unchangeStroke.classList='unchange-stroke-res';
+            rowRes.appendChild(unchangeStroke);
+                const teamRes = document.createElement('span');
+                teamRes.classList='team-res';
+                teamRes.textContent = 'Команда:';
+                unchangeStroke.appendChild(teamRes);
+
+                const teamDesc = document.createElement('span');
+                 teamDesc.classList='team-desc';
+                unchangeStroke.appendChild(teamDesc);
+
+            rowRes.appendChild(unchangeStroke);
+                const floorRes = document.createElement('span');
+                floorRes.classList='floor-res';
+                floorRes.textContent = 'Этаж:';
+                unchangeStroke.appendChild(floorRes);
+
+                const floorNumber = document.createElement('span');
+                floorNumber.classList='floor-number';
+                unchangeStroke.appendChild(floorNumber);
+
+        const imgRes = document.createElement('div');
+        floorNumber.classList='img-res';
+        block.appendChild(imgRes);
+            const avatar = document.createElement('img');
+            avatar.classList='avatar-res';
+            imgRes.appendChild(avatar);
 
 
     // for (let employee of employees) {
