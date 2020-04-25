b817fb8678e4f62188b6686d30c14a1dcc068e49
avelarimrmt <49668773+avelarimrmt@users.noreply.github.com>
2020-04-25 16:33:01 +0500

    switch between floors of map

---
+++ intofficemap/js/floorSwitching.js
@@ -0,0 +1,64 @@
+const buttonUp = document.querySelector('.up-toggle');
+const buttonDown = document.querySelector('.down-toggle');
+const floorNumber = document.getElementById('floor-number');
+const floors = document.querySelectorAll('.floor-of-map');
+
+let currentFloor = 14;
+const minFloor = 14;
+const maxFloor = 15;
+
+function setFloorNumber(floor) {
+    floorNumber.textContent = floor.toString();
+}
+
+setFloorNumber(currentFloor);
+
+buttonUp.addEventListener('click', clickOnButtonUp);
+
+function clickOnButtonUp() {
+    //сменить на следующий спан floorNumbers(присвоить спану атрибут active)
+    //если конец спанов, то сделать кнопку buttonUp disabled
+    //убрать disabled у кнопки buttonDown
+
+    if (currentFloor === maxFloor)
+        buttonUp.disabled = true;
+    else {
+        currentFloor++;
+        setFloorNumber(currentFloor);
+        for (let floor of floors) {
+            if (floor.getAttribute('data-floor') === 'active')
+                floor.setAttribute('data-floor', '');
+        }
+
+        for (let floor of floors) {
+            if (floor.getAttribute('floor-number') === currentFloor.toString())
+                floor.setAttribute('data-floor', 'active');
+        }
+        buttonDown.disabled = false;
+        if (currentFloor === maxFloor)
+            buttonUp.disabled = true;
+    }
+}
+
+buttonDown.addEventListener('click', clickOnButtonDown);
+
+function clickOnButtonDown() {
+    if (currentFloor === minFloor)
+        buttonDown.disabled = true;
+    else {
+        currentFloor--;
+        setFloorNumber(currentFloor);
+        for (let floor of floors) {
+            if (floor.getAttribute('data-floor') === 'active')
+                floor.setAttribute('data-floor', '');
+        }
+
+        for (let floor of floors) {
+            if (floor.getAttribute('floor-number') === currentFloor.toString())
+                floor.setAttribute('data-floor', 'active');
+        }
+        buttonUp.disabled = false;
+        if (currentFloor === minFloor)
+            buttonDown.disabled = true;
+    }
+}
