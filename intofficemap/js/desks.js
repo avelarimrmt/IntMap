77c741ee98f41f66b3aafa39f8a078b4255649c5
DRed <dima.redkin.1999@gmail.com>
2020-04-26 17:15:54 +0500

    change pseudo class hover to custom class hover. Add this class on desk on mouseover

--- intofficemap/js/desks.js
+++ intofficemap/js/desks.js
@@ -1,20 +1,34 @@
 const desks = document.querySelectorAll(".light-table");
 
+
+for (let desk of desks) {
+    desk.addEventListener('mouseover', () => {
+        desk.classList += ' hover';
+    });
+    desk.addEventListener('mouseout', () => {
+        desk.classList = desk.classList.toString().replace(' hover', '')
+    });
+}
+
 export function getAllDesks() {
     return desks;
 }
 
-export function highlightDeskById(deskId, color = 'B58F1C') {
+export function highlightDeskById(deskId, color = '#B58F1C') {
     for (let desk of desks) {
         let currentId = parseInt(desk.getAttribute("data-id"));
         if (currentId === deskId) {
-            desk.style.fill = color;
+            desk.setAttribute('fill', color);
+
+            desk.addEventListener('mouseover', () => {
+                desk.classList = desk.classList.toString().replace(' hover', '')
+            });
         }
     }
 }
 
 export function fillAllDesksWithInitialColor() {
     for (let desk of desks) {
-        desk.style.fill = 'FFC617';
+        desk.setAttribute('fill', '#FFC617');
     }
 }
