e0a43c51a1b23ebb6abe9563854f613e1f56f1f2
DRed <dima.redkin.1999@gmail.com>
2020-05-16 19:35:58 +0500

    add sha256 script to calculate hash value

--- intofficemap/js/authenticate.js
+++ intofficemap/js/authenticate.js
@@ -4,9 +4,13 @@ const tokenKey = "accessToken";
 async function getTokenAsync() {
 
     // получаем данные формы и фомируем объект для отправки
+    const login = document.getElementById("login-input").value;
+    const password = document.getElementById("password-input").value;
+    const hashValue = window.sha256(password);
+
     const formData = new FormData();
-    formData.append("login", document.getElementById("login-input").value);
-    formData.append("password", document.getElementById("password-input").value);
+    formData.append("login", login);
+    formData.append("password", hashValue);
 
     // отправляет запрос и получаем ответ
     const response = await fetch("https://offficemap.azurewebsites.net/api/account/get-token", {
